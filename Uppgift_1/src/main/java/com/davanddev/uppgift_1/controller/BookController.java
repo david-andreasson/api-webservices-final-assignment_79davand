12225365096d05c335a461ef04d77dfaf99f2f41
david-andreasson <79davand@gafe.molndal.se>
2025-03-14 06:50:19 +0100

    Created a minimal Bookcontroller according to MVP

---
+++ Uppgift_1/src/main/java/com/davanddev/uppgift_1/controller/BookController.java
@@ -0,0 +1,37 @@
+package com.davanddev.uppgift_1.controller;
+
+import com.davanddev.uppgift_1.model.Book;
+import com.davanddev.uppgift_1.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookService bookService;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        return bookService.getBookById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        Book createdBook = bookService.createBook(book);
+        return ResponseEntity.created(URI.create("/books/" + createdBook.getId()))
+                .body(createdBook);
+    }
+}
