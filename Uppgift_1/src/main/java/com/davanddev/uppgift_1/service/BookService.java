5d6bf8e7bb53ae1f059a8a938463a0bc9f79dc23
david-andreasson <79davand@gafe.molndal.se>
2025-03-14 07:45:33 +0100

    Created a minimal BookService with some generated example-books.

---
+++ Uppgift_1/src/main/java/com/davanddev/uppgift_1/service/BookService.java
@@ -0,0 +1,57 @@
+package com.davanddev.uppgift_1.service;
+
+import com.davanddev.uppgift_1.model.Book;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class BookService {
+
+    private final List<Book> books = new ArrayList<>();
+    private final AtomicLong idGenerator = new AtomicLong();
+
+    public BookService() {
+        books.add(new Book(1L, "Neuromancer", "William Gibson", 1984));
+        books.add(new Book(2L, "Count Zero", "William Gibson", 1986));
+        books.add(new Book(3L, "Mona Lisa Overdrive", "William Gibson", 1988));
+        books.add(new Book(4L, "Snow Crash", "Neal Stephenson", 1992));
+        books.add(new Book(5L, "Virtual Light", "William Gibson", 1993));
+        books.add(new Book(6L, "Islands in the Net", "Bruce Sterling", 1988));
+        books.add(new Book(7L, "Schismatrix", "Bruce Sterling", 1985));
+        books.add(new Book(8L, "Hardwired", "Walter Jon Williams", 1989));
+        books.add(new Book(9L, "When Gravity Fails", "George Alec Effinger", 1986));
+        books.add(new Book(10L, "Altered Carbon", "Richard K. Morgan", 2002));
+        books.add(new Book(11L, "Idoru", "William Gibson", 1996));
+        books.add(new Book(12L, "Cyberpunk", "Bruce Bethke", 1983));
+        books.add(new Book(13L, "Trouble and Her Friends", "Melissa Scott", 1994));
+        books.add(new Book(14L, "Synners", "Pat Cadigan", 1991));
+        books.add(new Book(15L, "Halting State", "Charles Stross", 2007));
+        books.add(new Book(16L, "Accelerando", "Charles Stross", 2005));
+        books.add(new Book(17L, "Software", "Rudy Rucker", 1982));
+        books.add(new Book(18L, "Wetware", "Rudy Rucker", 1988));
+        books.add(new Book(19L, "Freeware", "Rudy Rucker", 1997));
+        books.add(new Book(20L, "Shockwave Rider", "John Brunner", 1975));
+
+        idGenerator.set(20);
+    }
+
+    public List<Book> getAllBooks() {
+        return books;
+    }
+
+    public Optional<Book> getBookById(Long id) {
+        return books.stream()
+                .filter(book -> book.getId().equals(id))
+                .findFirst();
+    }
+
+    public Book createBook(Book book) {
+        book.setId(idGenerator.incrementAndGet());
+        books.add(book);
+        return book;
+    }
+}
