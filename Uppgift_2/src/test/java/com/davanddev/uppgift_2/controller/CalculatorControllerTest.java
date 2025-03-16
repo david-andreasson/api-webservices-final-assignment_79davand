72f4ad7e4bae9154ec70412a3289391d504b305a
david-andreasson <79davand@gafe.molndal.se>
2025-03-16 06:08:09 +0100

    Added testclass for CalculatorController and added another small test in main to get 100% coverage

--- Uppgift_2/src/test/java/com/davanddev/uppgift_2/controller/CalculatorControllerTest.java
+++ Uppgift_2/src/test/java/com/davanddev/uppgift_2/controller/CalculatorControllerTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
