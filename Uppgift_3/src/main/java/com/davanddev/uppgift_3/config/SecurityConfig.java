1bd842c8366c8e80c28a26b4e136954ab3bb0fd5
david-andreasson <79davand@gafe.molndal.se>
2025-03-17 06:54:19 +0100

    Added security with Basic Authentication

---
+++ Uppgift_3/src/main/java/com/davanddev/uppgift_3/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.davanddev.uppgift_3.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        // Using {noop} so that we don't need a password encoder for this mvp assignment
+        UserDetails user = User
+                .withUsername("soapuser")
+                .password("{noop}soappassword")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
