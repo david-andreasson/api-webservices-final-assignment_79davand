daf0fc4241da7dd58f2d44a8780d3314b6795a5e
david-andreasson <79davand@gafe.molndal.se>
2025-03-18 14:53:33 +0100

    Created initial folder structure

--- Uppgift_6/src/main/java/com/davanddev/uppgift_6/Uppgift6Application.java
+++ Uppgift_6/src/main/java/com/davanddev/uppgift_6/Uppgift6Application.java
@@ -1,4 +1,4 @@
-package edu.campusmolndal.uppgift_6;
+package com.davanddev.uppgift_6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
