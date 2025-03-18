9c23ff889c465fe24da8b8c510c534d3c27daf3f
david-andreasson <79davand@gafe.molndal.se>
2025-03-18 14:55:39 +0100

    Created simple User-model

---
+++ Uppgift_6/src/main/java/com/davanddev/uppgift_6/model/User.java
@@ -0,0 +1,13 @@
+package com.davanddev.uppgift_6.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String username;
+    private String email;
+    private String password;
+}
